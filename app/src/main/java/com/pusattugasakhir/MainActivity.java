a88241db16f29c1feb48b19597bea0e15eecb41c
Fairenn <faizabdullah2708@gmail.com>
2022-09-12 21:42:16 +0700

    tes comit

--- app/src/main/java/com/pusattugasakhir/MainActivity.java
+++ app/src/main/java/com/pusattugasakhir/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//komen ini
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
