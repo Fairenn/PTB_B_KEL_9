c20230a01d149a4afba36fa89bb998004e577d2a
Hafiz2028 <91659690+Hafiz2028@users.noreply.github.com>
2022-09-21 17:36:10 +0700

    apis update 4

--- app/src/main/java/com/pusattugasakhir/MainActivity.java
+++ app/src/main/java/com/pusattugasakhir/MainActivity.java
@@ -1,14 +1,57 @@
 package com.pusattugasakhir;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 //komen ini
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_logbook:
+                        selectedFragment = new LogbookFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                    case R.id.nav_semhas:
+                        selectedFragment = new SemhasFragment();
+                        break;
+                    case R.id.nav_sidangta:
+                        selectedFragment = new SidangTAFragment();
+                        break;
+
+
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+
     }
 }
