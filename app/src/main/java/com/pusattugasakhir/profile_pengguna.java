6fc9b50fa195476748b75d34bd457a459e348933
Fairenn <faizabdullah2708@gmail.com>
2022-09-22 19:49:33 +0700

    login dan profil pengguna

---
+++ app/src/main/java/com/pusattugasakhir/profile_pengguna.java
@@ -0,0 +1,14 @@
+package com.pusattugasakhir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile_pengguna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_pengguna);
+    }
+}
