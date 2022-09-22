841ae3ec71fc79d748d3e8c6f072f5ee8d28f0fd
Fairenn <faizabdullah2708@gmail.com>
2022-09-22 22:26:25 +0700

    input peserta semhas

---
+++ app/src/main/java/com/pusattugasakhir/input_peserta_semhas.java
@@ -0,0 +1,14 @@
+package com.pusattugasakhir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class input_peserta_semhas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_peserta_semhas);
+    }
+}
