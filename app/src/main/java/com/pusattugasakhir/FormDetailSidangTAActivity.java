c6b160a9638acb1bc8280b85146879afcbae3616
Hafiz2028 <91659690+Hafiz2028@users.noreply.github.com>
2022-10-14 19:34:36 +0700

    apis update 9

--- app/src/main/java/com/pusattugasakhir/FormDetailSidangTAActivity.java
+++ app/src/main/java/com/pusattugasakhir/FormDetailSidangTAActivity.java
@@ -2,7 +2,10 @@ package com.pusattugasakhir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormDetailSidangTAActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FormDetailSidangTAActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_detail_sidang_taactivity);
+        Button button = findViewById(R.id.btnokdetailsidangta);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FormDetailSidangTAActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
