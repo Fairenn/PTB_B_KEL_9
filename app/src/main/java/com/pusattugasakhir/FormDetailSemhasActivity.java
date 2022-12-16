3cf07d7a7c87c69f0d3216a2fcc33e178f764fe1
Dwiky RF <dwikyrf@gmail.com>
2022-12-17 03:03:58 +0700

    Dwiky

--- app/src/main/java/com/pusattugasakhir/FormDetailSemhasActivity.java
+++ app/src/main/java/com/pusattugasakhir/FormDetailSemhasActivity.java
@@ -2,7 +2,10 @@ package com.pusattugasakhir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormDetailSemhasActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FormDetailSemhasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_detail_semhas);
+        Button button = findViewById(R.id.btnokdetailsemhas);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FormDetailSemhasActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
