71956117f12beb62924777099e9628d2772cc3e3
Dwiky RF <dwikyrf@gmail.com>
2022-12-28 12:01:36 +0700

    Dwiky

--- app/src/main/java/com/pusattugasakhir/ProfileFragment.java
+++ app/src/main/java/com/pusattugasakhir/ProfileFragment.java
@@ -1,7 +1,10 @@
 package com.pusattugasakhir;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,8 +13,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.pusattugasakhir.api.APIClient;
+import com.pusattugasakhir.pojo.LogoutResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,11 +33,12 @@ public class ProfileFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private Button btnLogout;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private String usn,gettoken,token;
+    Button btnLogout;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -65,7 +75,45 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootview = LayoutInflater.from(getContext()).inflate(R.layout.fragment_profile, container, false);
+        btnLogout =rootview.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+        return rootview;
+    }
+
+
+    public void logout(){
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("userkey", Context.MODE_PRIVATE);
+        gettoken = sharedPreferences.getString("token", "");
+        token = "bearer " + gettoken;
+
+        retrofit2.Call<LogoutResponse> logoutResponseCall = APIClient.getUserInterface().userLogout(token);
+        logoutResponseCall.enqueue(new Callback<LogoutResponse>() {
+            @Override
+            public void onResponse(Call<LogoutResponse> call, Response<LogoutResponse> response) {
+
+                if (response.code() == 200){
+                    if (response.isSuccessful()){
+                        Intent intent = new Intent(getActivity(), login.class);
+                        startActivity(intent);
+                        Toast.makeText(getActivity(),response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                        sharedPreferences.edit().clear().apply();
+                        startActivity(intent);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LogoutResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Throwable"+t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
