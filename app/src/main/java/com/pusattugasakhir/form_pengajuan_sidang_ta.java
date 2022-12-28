0bd3d3725a914ed225f8bca985437bdc1f7b0ef9
Fairenn <faizabdullah2708@gmail.com>
2022-12-28 14:06:57 +0700

    firebase test

--- app/src/main/java/com/pusattugasakhir/form_pengajuan_sidang_ta.java
+++ app/src/main/java/com/pusattugasakhir/form_pengajuan_sidang_ta.java
@@ -1,8 +1,10 @@
 package com.pusattugasakhir;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -11,16 +13,43 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class form_pengajuan_sidang_ta extends AppCompatActivity {
     private Button btnajukansidangta;
+    private static final String TAG ="form_pengajuan_sidang_ta-Debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_pengajuan_sidang_ta);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @SuppressLint("LongLogTag")
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, token);
+                        Toast.makeText(form_pengajuan_sidang_ta.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         Button button = findViewById(R.id.btnajukansidangta);
         button.setOnClickListener(new View.OnClickListener() {
