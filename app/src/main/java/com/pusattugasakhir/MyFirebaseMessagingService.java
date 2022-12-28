d2277a4b68f6d529ec386ffa0e4416e8231bec30
Fairenn <faizabdullah2708@gmail.com>
2022-12-28 09:10:36 +0700

    firebase test

--- app/src/main/java/com/pusattugasakhir/MyFirebaseMessagingService.java
+++ app/src/main/java/com/pusattugasakhir/MyFirebaseMessagingService.java
@@ -1,8 +1,16 @@
 package com.pusattugasakhir;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -21,6 +29,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void displayNotification(RemoteMessage.Notification notification) {
+
+        Intent intent = new Intent(this, FormDetailSidangTAActivity.class);
+        startActivity(intent);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext(),"CH1")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentText("Sidang TA telah Di ajukan")
+                .setContentTitle("Sidang TA")
+                .setAutoCancel(true)
+                . setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("CH1","Notifikasi",NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(0,notificationBuilder.build());
+
     }
 
 }
